85f5713d8631974614d7e9c54b79d3b6e0c0fd28
philippzach <mail@philippzach.com>
2019-10-12 15:11:43 +0200

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,10 @@
 import React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../components/layout/2"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
+const NotFoundPage = ({ data }) => (
+  <Layout data={data.contact} heading="Not Found">
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
@@ -12,3 +12,18 @@ const NotFoundPage = () => (
 )
 
 export default NotFoundPage
+
+export const data = graphql`
+  query {
+    contact: file(relativePath: { eq: "contactus.jpg" }) {
+      childImageSharp {
+        fluid(
+          maxWidth: 1900
+          traceSVG: { background: "#ffff", color: "#2f5495" }
+        ) {
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`
